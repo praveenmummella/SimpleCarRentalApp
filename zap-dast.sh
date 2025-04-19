fbee771ed12a9d2071da1af55a0140cfafa343a0
praveenmummella <80162116+praveenmummella@users.noreply.github.com>
2025-04-19 13:07:43 +0530

    Update zap-dast.sh

--- zap-dast.sh
+++ zap-dast.sh
@@ -4,7 +4,7 @@ set -e
 ZAP_REPORT_DIR="zap-report"
 mkdir -p $ZAP_REPORT_DIR
 
-TARGET_URL="http://simplecarrentalapp-stagging.eba-xyz.us-east-1.elasticbeanstalk.com"
+TARGET_URL="http://simplecarrentalapp-stagging.eba-tz5hr59y.us-east-1.elasticbeanstalk.com/"
 
 docker run -v $(pwd)/$ZAP_REPORT_DIR:/zap/wrk/:rw \
     ghcr.io/zaproxy/zaproxy:stable zap-baseline.py \
